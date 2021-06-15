22d1ce00d7987cd13d01a5852a5c68169a47a6ae
Felipe Norato Lacerda <fnoratol@gmail.com>
2021-06-15 18:49:04 -0300

    create design-system lib

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/angular-tips', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/angular-tips',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/design-system',
+  ],
 };
