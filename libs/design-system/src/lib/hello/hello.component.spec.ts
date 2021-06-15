2fa23b1fd5fc3d5ed8d7acb233bbf51af9cfb7ef
Felipe Norato Lacerda <fnoratol@gmail.com>
2021-06-15 19:23:28 -0300

    testing lib

--- libs/design-system/src/lib/hello/hello.component.spec.ts
+++ libs/design-system/src/lib/hello/hello.component.spec.ts
@@ -1,25 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
 
 import { HelloComponent } from './hello.component';
 
 describe('HelloComponent', () => {
-  let component: HelloComponent;
-  let fixture: ComponentFixture<HelloComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HelloComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HelloComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  let component: Spectator<HelloComponent>;
+  const createComponent = createComponentFactory(HelloComponent);
 
   it('should create', () => {
+    component = createComponent();
     expect(component).toBeTruthy();
   });
 });
